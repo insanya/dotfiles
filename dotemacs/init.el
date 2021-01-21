7ae9b5e164558495f7996cf7ac41e4acd2a90769
insanya <insanya@protonmail.com>
2021-01-21 22:02:28 +0000

    rework

--- dotemacs/init.el
+++ dotemacs/init.el
@@ -1,39 +1,36 @@
-;; Set custom file.
+;;; init.el --- Load the full configuration -*- lexical-binding: t -*-
+;;; Commentary:
+
+;; Configuration bootstrap
+
+;;; Code:
+
+;;(setq debug-on-error t)
+
+(let ((normal-gc-cons-threshold (* 20 1024 1024))
+      (init-gc-cons-threshold (* 128 1024 1024)))
+  (setq gc-cons-threshold init-gc-cons-threshold)
+  (add-hook 'emacs-startup-hook
+            (lambda () (setq gc-cons-threshold normal-gc-cons-threshold))))
+
 (setq-default custom-file (expand-file-name "custom.el" user-emacs-directory))
-(when (file-exists-p custom-file)
-  (load custom-file))
+(when (file-exists-p custom-file) (load custom-file))
 
-;; Make all commands of the “package” module present.
 (require 'package)
-(add-to-list 'package-archives
-             '("melpa" . "http://melpa.org/packages/") t)
-;; (add-to-list 'package-archives
-;;              '("org" . "http://orgmode.org/elpa/") t)
-
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
 (package-initialize)
 (package-refresh-contents)
 
-;; use-package configuration.
 (unless (package-installed-p 'use-package)
   (package-install 'use-package))
-
 (require 'use-package)
-
-;; Always ensures packages are in the system.
 (setq use-package-always-ensure t)
 (setq use-package-always-defer t)
 
-;; Automatic package update.
-(use-package auto-package-update
-  :custom
-  (auto-package-update-delete-old-versions t)
-  (auto-package-update-hide-results t)
-  :config
-  (auto-package-update-maybe))
-
-;; Disable Package Signature Check
 (setq package-check-signature nil)
 
-;; Tangle configuration
 (org-babel-load-file (expand-file-name "README.org" user-emacs-directory))
 (put 'dired-find-alternate-file 'disabled nil)
+
+(provide 'init)
+;;; init.el ends here
